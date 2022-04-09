6fe425484ca6dbbac660914fd07fa14e7a57c6c2
Fernanda Borges <91858227+fernanda-borg3s@users.noreply.github.com>
2022-04-09 20:42:48 -0300

    Add files via upload

---
+++ portifolio.js
@@ -0,0 +1,11 @@
+
+let navLinks = document.getElementById('navLinks');
+
+function showMenu(){
+    navLinks.style.width = "240px";
+    navLinks.style.display = "block";
+}
+function hideMenu(){
+    navLinks.style.width = "0";
+    navLinks.style.display = "none";
+}
